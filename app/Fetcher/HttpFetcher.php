a3929425ef2aa7f3018637a403b93181d9943f55
Dmitry <kdv@skbis.ru>
2023-02-21 22:16:38 +0300

    More clear example

--- app/Fetcher/HttpFetcher.php
+++ app/Fetcher/HttpFetcher.php
@@ -5,6 +5,7 @@ namespace App\Fetcher;
 use App\Contracts\Fetcher;
 use App\Exception\ConnectionException;
 
+// Possible more effective to use a singleton pattern to avoid instance reinitialize
 class HttpFetcher implements Fetcher
 {
 
@@ -21,8 +22,7 @@ class HttpFetcher implements Fetcher
      */
     public function fetch(string $url): string
     {
-        // TODO: Implement http fetch() method
         // Guzzle still strongly recommended https://docs.guzzlephp.org/en/stable/
-        return '';
+        return '<html lang="en"></html>';
     }
 }
