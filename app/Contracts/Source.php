e9c06a124072034703398ae29b01e74793dc7101
Dmitry <kdv@skbis.ru>
2023-02-21 22:15:30 +0300

    Add getType method

--- app/Contracts/Source.php
+++ app/Contracts/Source.php
@@ -3,11 +3,14 @@ declare(strict_types=1);
 
 namespace App\Contracts;
 
+use App\Sources\SourceType;
 use App\Sources\Strategies\Strategy;
 
 interface Source
 {
     public function addStrategy(Strategy $strategy): self;
 
-    public function parseMedia(string $page): array;
+    public function parseMedia(string $url): array;
+
+    public function getType(): SourceType;
 }
